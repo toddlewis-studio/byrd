ee7fafbfe48e904f32b53bd07ce3427cbab1748a
Brayan Byrdsong <brayanbyrdsong@Brayans-Mini.lan>
2024-04-07 18:12:43 -0400

    added services n stuff

--- test/www/index.js
+++ test/www/index.js
@@ -1,16 +1,45 @@
 const byrd = require('../../lib/byrd.js')
 
-const homeRoute = () => {
-  const testId = byrd.event.click(el => {
-    el.innerText = 'Works!'
-    document.title = 'Works!'
-  })
+document.title = 'Byrd - UI Library'
+byrd.data`counter`(0)
+
+byrd.register('main.counter', () => {
+  const val = byrd.data`counter`() + 1
+  byrd.data`counter`(val)
+  return val
+})
+
+byrd.element('home', () => {
+  const counterId = byrd.event.guid()
+  const testId = byrd.event.click(el => 
+    document.getElementById(counterId).innerText = 
+      byrd.service`main.counter`()
+  )
   return `
-    <h1>Byrd Engine</h1>
+    <h1>Byrd Engine <span id="${counterId}">${byrd.data`counter`()}</span></h1>
     <button id="${testId}">Test</button>
+    <a href="#other">Other</a>
   `
-}
+})
+
+byrd.element('other', () => {
+  byrd.service`main.counter`()
+  return `
+    <h1>Other <span>${byrd.data`counter`()}</span></h1>
+    <br>
+    <br>
+    <br>
+    <br>
+    <a href="#">Home</a>
+  `
+})
 
 const app = document.createElement('div')
 document.body.appendChild(app)
-byrd.init(app, {'#': homeRoute})
+byrd.init(
+  app, 
+  {
+    '#': byrd.el('home'),
+    '#other': byrd.el('other')
+  }
+)
